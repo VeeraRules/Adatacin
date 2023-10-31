59f9c32db3bdd7644d8b1719d000150f7ed57aeb
VeeraRules <veeravetripandiyan@gmail.com>
2023-10-31 16:58:01 +0530

    Javademo

---
+++ src/org/git/VeeragitDemo.java
@@ -0,0 +1,18 @@
+package org.git;
+
+import java.util.Iterator;
+
+public class VeeragitDemo {
+
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println(i);
+			
+		}
+		
+	}
+	
+}
